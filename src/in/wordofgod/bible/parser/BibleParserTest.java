0b61c43b024b4d7b3d918581bfeccce1a4d3ce30
Yesudas S <yesudas.rs@gmail.com>
2023-09-10 16:49:47 +0530

    updated

--- src/in/wordofgod/bible/parser/BibleParserTest.java
+++ src/in/wordofgod/bible/parser/BibleParserTest.java
@@ -6,11 +6,11 @@ import in.wordofgod.bible.parser.vosgson.Verse;
 
 public class BibleParserTest {
 
-	public static String theWordBibleFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\taOV.ont";
-	public static String bibleFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\taOV.txt";
-	public static String bookNamesFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\taOV-book-names.ini";
-	public static String shortBookNamesFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\taOV-book-short-names.ini";
-	public static String bibleInfoFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\taOV-information.ini";
+	public static String theWordBibleFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\TBSI.ont";
+	public static String bibleFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\TBSI.txt";
+	public static String bookNamesFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\TBSI-book-names.ini";
+	public static String shortBookNamesFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\TBSI-book-short-names.ini";
+	public static String bibleInfoFilePath = "D:\\WOG\\synched-wog-bibles\\bibles-texts\\My-Source\\Tamil\\TBSI-information.ini";
 
 	public static void main(String[] args) {
 		loadWordOfGodBible();
